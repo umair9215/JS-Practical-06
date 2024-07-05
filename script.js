029ffe29a7e0e3d50e51a38c111b395c1410e966
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-07-05 15:55:02 -0700

    Js Practical 06

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// let nam = "    Hello World!";
+// console.log(nam.trim( ));
+
+
+// let nam = "Hello World!";
+// console.log(nam.substring(5, 10));
+
+// let nam = "Hello World!";
+// console.log(nam.substr(5));
+
+// let firstName = "hello-world";
+// console.log(firstName.split("-"));
+
+// let secondArray = ["hello", "world"];
+// console.log(secondArray.join(" "));
+
+
+// let secondArray = "hello world";
+// console.log(secondArray.indexOf("o"));
+
+
+// let secondArray = "hello world world";
+// console.log(secondArray.lastIndexOf("world"));
+
+
+// let array = "world";
+// console.log(array.startsWith("wor"));
+
+
+// let word = "hello World";
+// console.log(word.endsWith("World"));
+
+
+// let word = "Hello world";
+// console.log(word.includes("o"));
